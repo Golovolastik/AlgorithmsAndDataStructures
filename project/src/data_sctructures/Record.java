9522f2235c77d30105d494debd2bd429b456c529
Golovolastik <yankovich.aliaksei@gmail.com>
2022-09-04 19:31:45 +0300

    record class

---
+++ project/src/data_sctructures/Record.java
@@ -0,0 +1,11 @@
+package data_sctructures;
+
+class Record {
+    int age;
+    String name;
+
+    public Record(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+}
---
+++ project/src/data_sctructures/Test.java
@@ -0,0 +1,8 @@
+package data_sctructures;
+
+public class Test {
+    public static void main(String[] args){
+        Record user1 = new Record(30, "Alex");
+        System.out.println(user1.age);
+    }
+}
